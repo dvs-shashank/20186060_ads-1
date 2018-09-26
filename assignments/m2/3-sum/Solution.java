4bbc7a44c01a1347b019b8c19c8b1233decef224
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-26 21:18:48 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- assignments/m2/3-sum/Solution.java
+++ assignments/m2/3-sum/Solution.java
@@ -4,44 +4,44 @@ import java.util.Arrays;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int size = scan.nextInt();
-		int count = 0;
-		int[] threesome = new int[size];
-		for (int i = 0; i < size; i++) {
-			threesome[i] = scan.nextInt();
-		}
-		Arrays.sort(threesome);
-		for (int i = 0; i < size - 2; i++) {
-			int j = i + 1;
-			int k = size - 1;
-			while (j < k) {
-				if (threesome[i] + threesome[j]
-					+ threesome[k] == 0) {
-					count++;
-					j++;
-					k--;
-				} else if (threesome[i] + threesome[j]
-					+ threesome[k] < 0) {
-					j++;
-				} else {
-					k--;
-				}
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int count = 0;
+        int[] threesome = new int[size];
+        for (int i = 0; i < size; i++) {
+            threesome[i] = scan.nextInt();
+        }
+        Arrays.sort(threesome);
+        for (int i = 0; i < size - 2; i++) {
+            int j = i + 1;
+            int k = size - 1;
+            while (j < k) {
+                if (threesome[i] + threesome[j]
+                        + threesome[k] == 0) {
+                    count++;
+                    j++;
+                    k--;
+                } else if (threesome[i] + threesome[j]
+                           + threesome[k] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
 
-			}
-		}
-		System.out.println(count);
-	}
+            }
+        }
+        System.out.println(count);
+    }
 }
