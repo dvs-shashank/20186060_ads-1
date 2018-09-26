3d32deafa51f7462d73de860c0ff49b8ecdf2edd
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-26 21:17:07 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m2/3-sum/Solution.java
+++ assignments/m2/3-sum/Solution.java
@@ -1,6 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
 class Solution {
-	public static void main(String[] args) {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int size = scan.nextInt();
 		int count = 0;
@@ -13,11 +28,13 @@ class Solution {
 			int j = i + 1;
 			int k = size - 1;
 			while (j < k) {
-				if (threesome[i] + threesome[j] + threesome[k] == 0) {
+				if (threesome[i] + threesome[j] +
+					threesome[k] == 0) {
 					count++;
 					j++;
 					k--;
-				} else if (threesome[i] + threesome[j] + threesome[k] < 0) {
+				} else if (threesome[i] + threesome[j] +
+					threesome[k] < 0) {
 					j++;
 				} else {
 					k--;
