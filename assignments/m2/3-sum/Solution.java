a5ee3655d9fb1b1100d82f17857d3d0a2b6bb8b8
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-26 21:11:32 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ assignments/m2/3-sum/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int size = scan.nextInt();
+		int count = 0;
+		int[] threesome = new int[size];
+		for (int i = 0; i < size - 2; i++) {
+			int j = i + 1;
+			int k = size - 1;
+			while (j < k) {
+				if (threesome[i] + threesome[j] + threesome[k] == 0) {
+					count++;
+					j++;
+					k--;
+				} else if (threesome[i] + threesome[j] + threesome[k] < 0) {
+					j++;
+				} else {
+					k--;
+				}
+
+			}
+		}
+		System.out.println(count);
+	}
+}
