ea3c243e9eaa67f213b730cd85b101b99dbb12b3
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-27 20:33:13 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public final class Solution {
+    private Solution() {
+    }
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = Integer.parseInt(sc.nextLine());
+        Balanceparenthesis balance = new Balanceparenthesis();
+        while (size > 0) {
+            String input = sc.next();
+            if (balance.checkParanthesis(input)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            size--;
+        }
+    }
+}
