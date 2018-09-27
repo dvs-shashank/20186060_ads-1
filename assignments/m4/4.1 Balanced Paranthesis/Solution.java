b1a6ffa74507f6650648a8e5c68ec7f0ef5d9b59
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-27 20:36:29 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
+/**
+ * class for Solution.
+ */
 public final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
+
     }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = Integer.parseInt(sc.nextLine());
