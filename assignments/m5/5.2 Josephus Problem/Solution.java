2a609236eb11fe0f7d6cd72f3344d3a7d93d5338
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-30 22:42:13 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m5/5.2 Josephus Problem/Solution.java
+++ assignments/m5/5.2 Josephus Problem/Solution.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
+/**
+ * class solution.
+ */
 public final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
     }
-
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         Queue queueObj = new Queue();
@@ -28,29 +38,63 @@ public final class Solution {
         }
     }
 }
+/**
+ * class queue.
+ */
 class Queue {
-
+    /**
+     * size.
+     */
     private int size;
+    /**
+     * queue node.
+     */
     private Node queue;
+    /**
+     * head node.
+     */
     private Node head;
+    /**
+     * nested class node.
+     */
     class Node {
+        /**
+         * data.
+         */
         private int data;
+        /**
+         * link.
+         */
         private Node link;
+        /**
+         * Constructs the object.
+         */
         Node() {
             this.data = 0;
             this.link = null;
         }
+        /**
+         * Constructs the object.
+         *
+         * @param      data1  The data 1
+         */
         Node(final int data1) {
             this.data = data1;
             this.link = null;
         }
     }
-
+    /**
+     * Constructs the object.
+     */
     Queue() {
         queue = new Node();
         size = 0;
     }
-
+    /**
+     * enqueing the data.
+     *
+     * @param      data  The data
+     */
     public void enQueue(final int data) {
         if (size == 0) {
             head = new Node(data);
@@ -63,6 +107,11 @@ class Queue {
         size++;
         return;
     }
+    /**
+     * dequeuing the data.
+     *
+     * @return     data.
+     */
     public int deQueue() {
         Node current = head;
         if (size == 1) {
@@ -82,10 +131,19 @@ class Queue {
         size--;
         return data;
     }
-
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
     public boolean isEmpty() {
         return size == 0;
     }
+    /**
+     * Gets the size.
+     *
+     * @return     The size.
+     */
     public int getSize() {
         return size;
     }
