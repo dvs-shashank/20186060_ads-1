81369a48503374df460f0d69282e4a13e898aa80
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-10-06 12:33:55 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m12/Assignment-1/Solution.java
+++ assignments/m12/Assignment-1/Solution.java
@@ -3,51 +3,68 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	static int qstud;
-	static int vacancy;
-	static int open;
-	static int bc;
-	static int sc;
-	static int st;
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * stud qualified.
+     */
+    static int qstud;
+    /**
+     * vacancy.
+     */
+    static int vacancy;
+    /**
+     * open.
+     */
+    static int open;
+    /**
+     * bc.
+     */
+    static int bc;
+    /**
+     * sc.
+     */
+    static int sc;
+    /**
+     * st.
+     */
+    static int st;
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner yuvi = new Scanner(System.in);
-		Insertion insert = new Insertion();
-		int qualifiedStudents = Integer.parseInt(yuvi.nextLine());
-		qstud = qualifiedStudents;
-		int vacancies = Integer.parseInt(yuvi.nextLine());
-		vacancy = vacancies;
-		int unreservedVacancies = Integer.parseInt(yuvi.nextLine());
-		open = unreservedVacancies;
-		int bcVacancies = Integer.parseInt(yuvi.nextLine());
-		bc = bcVacancies;
-		int scVacancies = Integer.parseInt(yuvi.nextLine());
-		sc = scVacancies;
-		int stVacancies = Integer.parseInt(yuvi.nextLine());
-		st = stVacancies;
-		while (yuvi.hasNext()) {
-			String studentInfo = yuvi.nextLine();
-			String[] studentInfoArr = studentInfo.split(",");
-			insert.addStudents(new Student(studentInfoArr[0],
-			                               studentInfoArr[1],
-			                               Integer.parseInt(studentInfoArr[2]),
-			                               Integer.parseInt(studentInfoArr[3]),
-			                               Integer.parseInt(studentInfoArr[4]),
-			                               Integer.parseInt(studentInfoArr[5]),
-			                               studentInfoArr[6]));
-		}
-		insert.sort();
-		System.out.println(insert);
-	}
-
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner yuvi = new Scanner(System.in);
+        Insertion insert = new Insertion();
+        int qualifiedStudents = Integer.parseInt(yuvi.nextLine());
+        qstud = qualifiedStudents;
+        int vacancies = Integer.parseInt(yuvi.nextLine());
+        vacancy = vacancies;
+        int unreservedVacancies = Integer.parseInt(yuvi.nextLine());
+        open = unreservedVacancies;
+        int bcVacancies = Integer.parseInt(yuvi.nextLine());
+        bc = bcVacancies;
+        int scVacancies = Integer.parseInt(yuvi.nextLine());
+        sc = scVacancies;
+        int stVacancies = Integer.parseInt(yuvi.nextLine());
+        st = stVacancies;
+        while (yuvi.hasNext()) {
+            String studentInfo = yuvi.nextLine();
+            String[] studentInfoArr = studentInfo.split(",");
+            insert.addStudents(new Student(studentInfoArr[0],
+                                           studentInfoArr[1],
+                                           Integer.parseInt(studentInfoArr[2]),
+                                           Integer.parseInt(studentInfoArr[3]),
+                                           Integer.parseInt(studentInfoArr[4]),
+                                           Integer.parseInt(studentInfoArr[5]),
+                                           studentInfoArr[6]));
+        }
+        insert.sort();
+        System.out.println(insert);
+    }
 }
